abe205fb71a346747e60df5dffa10f96a5a0bfeb
neyugn21 <thanhntph47600@gmail.com>
2024-06-29 12:47:43 +0700

    Tạo mới file index

---
+++ index2.php
@@ -0,0 +1,13 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>Đây là file 2</h3>
+</body>
+
+</html>
